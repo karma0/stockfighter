15b2ac3d6fbef279e45abdd11e89af064f470932
Bobby Larson <karma0@gmail.com>
2015-12-17 10:18:45 -0700

    Removed attempt at pulling in the README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup, find_packages
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
 
 setup(
   name = 'stockfighter-io',
