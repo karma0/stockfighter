b3dc0ae945f9512fc20f4cbe43da7f39b7487e17
Bobby Larson <karma0@gmail.com>
2015-12-16 22:24:00 -0700

    Restructured for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'stockfighter-io-trades',
+  packages = ['stockfighter-io-trades'],
+  version = '0.1',
+  description = 'An API wrapper for the Stockfighter.io Trades.Exec() system.',
+  author = 'Bobby Larson',
+  author_email = 'karma0@gmail.com',
+  url = 'https://github.com/karma0/stockfighter-io-trader',
+  download_url = 'https://github.com/karma0/stockfighter-io-trader/tarball/0.1', # I'll explain this in a second
+  keywords = ['stockfighter', 'exchange', 'trade'], # arbitrary keywords
+  classifiers = [],
+)
