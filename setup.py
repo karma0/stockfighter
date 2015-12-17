98b59a1d6d3b68809039f9075a1da834cbbe149c
Bobby Larson <karma0@gmail.com>
2015-12-16 23:15:12 -0700

    Bumped version to include requests dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stockfighter-io-trades',
   packages = ['stockfighter-io-trades'],
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'An API wrapper for the Stockfighter.io Trades.Exec() system.',
   author = 'Bobby Larson',
   author_email = 'karma0@gmail.com',
@@ -10,4 +10,7 @@ setup(
   download_url = 'https://github.com/karma0/stockfighter-io-trades/tarball/0.1', # I'll explain this in a second
   keywords = ['stockfighter', 'exchange', 'trades'], # arbitrary keywords
   classifiers = [],
+  install_requires = [
+    'requests'
+  ],
 )
