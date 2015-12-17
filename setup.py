b15bc37ae9a9e5e37d4d58ce178890831dea5413
Bobby Larson <karma0@gmail.com>
2015-12-17 10:15:49 -0700

    Fixed a number of issues with the packaging.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
 setup(
   name = 'stockfighter-io',
-  packages = ['stockfighter'],
-  version = '0.1.3',
+  packages = find_packages(exclude=['dist']),
+  version = '0.1.4',
   description = 'An API wrapper for Stockfighter.io',
   author = 'Bobby Larson',
   author_email = 'karma0@gmail.com',
-  url = 'https://github.com/karma0/stockfighter-io-trades',
+  url = 'https://github.com/karma0/stockfighter',
   download_url = 'https://github.com/karma0/stockfighter/tarball/0.1',
-  keywords = ['stockfighter', 'exchange', 'trades'], # arbitrary keywords
+  keywords = ['stockfighter', 'exchange', 'trades'],
   classifiers = [],
   install_requires = [
     'requests',
